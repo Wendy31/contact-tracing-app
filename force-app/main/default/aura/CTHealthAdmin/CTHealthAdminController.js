868fefa4a39c3d5400b1f5136e5fc7eb7059ae0f
Wendy <wendychung31@gmail.com>
2022-04-20 12:30:56 +0100

    selected tab and change headerTitle, and add function to create new record within the tabs

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
-
+    tabSelectHandler : function(component, event, helper) {
+        // gets the tab IDs
+        const selectedTabId = event.getParam("id");
+        // then set the headerTitles on the cmp
+        if(selectedTabId === "person"){
+            component.set("v.headerTitle", "Person View");
+        } else {
+            component.set("v.headerTitle", "Location View");
+        }
+        // then set scope
+        component.set("v.scope", selectedTabId);
     }
 })
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -1,5 +1,15 @@
 ({
-    myAction : function(component, event, helper) {
+    createRecord: function (component, event, helper) {
+        // use standard lightning event to show modal to create record
+        // use scope to determine which record to create
+        // then give the object names
+        // and fire the create record event
+        const createRecordEvent = $A.get("e.force:createRecord"); // call the createRecord event
+        const scope = component.get("v.scope");
+        createRecordEvent.setParams({
+            "entityApiName": scope === "person" ? "Person__c" : "Location__c"
+        });
+        createRecordEvent.fire();
 
     }
-})
+});
